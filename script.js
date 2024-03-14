a6d0c2d636ea513d35050bd1ca1725aa71753901
TheSilkFlower <smirnova100991@gmail.com>
2024-03-14 04:09:39 +0300

    add map

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-console.log('Hi');
+let map;
 
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center : { lat: 56.309348, lng: 44.028366 },
+    zoom: 18
+  });
+}
+
+initMap();
